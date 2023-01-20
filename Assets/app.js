78b8762f507585f1397b0261312752741e878244
p0ptartz <ericwm420@gmail.com>
2023-01-19 22:02:43 -0500

    added API key and fetch but getting 401

--- Assets/app.js
+++ Assets/app.js
@@ -1,6 +1,17 @@
 let currentDate = moment().format("MMMM DD, YYYY")
 const currentDateEl = document.querySelector("#current-date")
-
+let apiKey = "a4e384b892a2d03639abac10e9554496";
+const requestUrl = "http://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid={apiKey}"
 
 
 currentDateEl.innerHTML = currentDate
+
+fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    })
+
+
