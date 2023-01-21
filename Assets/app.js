31fc5b5c7cde71ec35599bc09cfd6daa013ec891
p0ptartz <ericwm420@gmail.com>
2023-01-20 21:01:15 -0500

    added 5day layout HTML CSS

--- Assets/app.js
+++ Assets/app.js
@@ -1,16 +1,18 @@
 let currentDate = moment().format("MMMM DD, YYYY")
 const currentDateEl = document.querySelector("#current-date")
-// let APIKey = "f6db8585ff8265ffbd2cbb997c9856a7";
-// let requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=f6db8585ff8265ffbd2cbb997c9856a7"
+const apiKey = "f6db8585ff8265ffbd2cbb997c9856a7";
 
 
 currentDateEl.innerHTML = currentDate
 
-
-
 function getWeather() {
+    currentWeather()
+    fiveDayWeather()
+}
+
+
+function currentWeather() {
     let city = document.querySelector("#city-input").value;
-    let apiKey = "f6db8585ff8265ffbd2cbb997c9856a7";
     let currentCity = document.querySelector(".city")
     let currentTemp = document.querySelector("#temp")
     let wind = document.querySelector("#wind")
@@ -24,15 +26,28 @@ function getWeather() {
         })
         .then(function (data) {
             console.log(data)
-            currentCity.innerHTML = data.name
+            currentCity.innerHTML = data.name + data.weather[0].icon
             currentTemp.innerHTML = data.main.temp + " °F"
             wind.innerHTML = data.wind.speed + " MPH"
             humidity.innerHTML = data.main.humidity + "%"
 
         })
         .catch(error => console.error(error));
-
 }
+
+function fiveDayWeather() {
+    let city = document.querySelector("#city-input").value;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+}
+
 
 
 
